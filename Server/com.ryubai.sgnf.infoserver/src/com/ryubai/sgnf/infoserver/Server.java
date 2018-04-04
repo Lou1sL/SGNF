4f6f658c90e00fc111def46523c56ef5f20bc124
留白 <156890093@qq.com>
2018-04-05 03:37:05 +0800

    asdas

---
+++ Server/com.ryubai.sgnf.infoserver/src/com/ryubai/sgnf/infoserver/Server.java
@@ -0,0 +1,71 @@
+package com.ryubai.sgnf.infoserver;
+
+import io.netty.bootstrap.ServerBootstrap;  
+import io.netty.channel.ChannelFuture;   
+import io.netty.channel.ChannelInitializer;   
+import io.netty.channel.ChannelOption;   
+import io.netty.channel.EventLoopGroup;   
+import io.netty.channel.nio.NioEventLoopGroup;   
+import io.netty.channel.socket.SocketChannel;   
+import io.netty.channel.socket.nio.NioServerSocketChannel;  
+  
+public class Server {
+	
+	public void bind(int port) throws Exception
+    {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();//线程组
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();//server启动管理配置
+            b.group(bossGroup, workGroup)
+            .channel(NioServerSocketChannel.class)
+            .option(ChannelOption.SO_BACKLOG, 1024)//最大客户端连接数为1024
+            .childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+
+                	//业务逻辑
+                	ch.pipeline().addLast(new ServerHandler());
+                    ServerBootstrap b = new ServerBootstrap();//server启动管理配置
+                                b.group(bossGroup, workGroup)
+                                .channel(NioServerSocketChannel.class)
+                                .option(ChannelOption.SO_BACKLOG, 1024)//最大客户端连接数为1024
+                                .childHandler(new ChannelInitializer<SocketChannel>() {
+                                    @Override
+                                    protected void initChannel(SocketChannel ch) throws Exception {
+                                        ch.pipeline().addLast(new ServerHandler());
+                                        
+                                        
+                                        
+                                        ch.pipeline().addLast(new LengthDecoder(1024,0,4,0,4));
+                                        ch.pipeline().addLast(new MessageDecoder());
+                                        ch.pipeline().addLast(new MessageEncoder());
+                                        ch.pipeline().addLast(new ServerHandler());
+                                        
+                                        
+                                    }
+                                });
+                                
+                }
+            });
+            ChannelFuture f = b.bind(port).sync();
+            if (f.isSuccess())
+            {
+                System.out.println("Server starts success at port:" + port);
+            }
+            f.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+	
+	
+    public static void main(String[] args) throws Exception
+    {
+        int port = 8086;
+        new Server().bind(port);
+    }
+}  
---
+++ Server/com.ryubai.sgnf.infoserver/src/com/ryubai/sgnf/infoserver/ServerHandler.java
@@ -0,0 +1,44 @@
+package com.ryubai.sgnf.infoserver;
+
+import io.netty.channel.ChannelHandlerContext;  
+import io.netty.channel.ChannelInboundHandlerAdapter;  
+  
+public class ServerHandler extends ChannelInboundHandlerAdapter{
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception  //当客户端连上服务器的时候会触发此函数
+    {
+        System.out.println("clinet:" + ctx.channel().id() + " join server");
+    }
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception//当客户端断开连接的时候触发函数
+    {
+        System.out.println("clinet:" + ctx.channel().id() + " leave server");
+        //User.onlineUser.remove(LoginDispatch.getInstance().user);
+    }
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception//当客户端发送数据到服务器会触发此函数
+    {
+    	/*
+        SocketModel message = (SocketModel) msg;
+        switch (message.getType()) {
+        case TypeProtocol.TYPE_LOGIN:
+            LoginDispatch.getInstance().dispatch(ctx, message);
+            break;
+        case TypeProtocol.TYPE_WIZARD:
+            WizardDispatch.getInstance().dispatch(ctx, message);
+            break;
+        case TypeProtocol.TYPE_USER:
+            UserDispatch.getInstance().dispatch(ctx, message);
+            break;
+        case TypeProtocol.TYPE_BATTLE:
+            BattleDispatch.getInstance().dispatch(ctx, message);
+        default:
+            break;
+        }*/
+    }
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception
+    {
+        cause.printStackTrace();
+    }
+}
