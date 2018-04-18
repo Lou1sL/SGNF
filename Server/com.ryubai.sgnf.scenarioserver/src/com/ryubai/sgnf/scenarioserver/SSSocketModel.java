b3f1a908e060560936e32e2e54732259bd59fcd8
留白 <156890093@qq.com>
2018-04-19 04:18:00 +0800

    ...

--- Server/com.ryubai.sgnf.scenarioserver/src/com/ryubai/sgnf/scenarioserver/SSSocketModel.java
+++ Server/com.ryubai.sgnf.scenarioserver/src/com/ryubai/sgnf/scenarioserver/SSSocketModel.java
@@ -4,16 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SSSocketModel {
-    public int command;
-    public int currentTick;
-    public List<Integer> message = new ArrayList<Integer>();
-    public List<Vec> vector = new ArrayList<Vec>();
-    
-    
-    public class Vec{
-    	public int tag = -1;
-    	public float x = 0;
-    	public float y = 0;
-    	public float z = 0;
-    }
+	public int command;
+	public int currentTick;
+	public List<Integer> message = new ArrayList<Integer>();
+	public List<Vector> vector = new ArrayList<Vector>();
+	
+	public SSSocketModel() {
+
+	}
+
+	public SSSocketModel(SSSocketModel bla) {
+		command = bla.command;
+		currentTick = bla.currentTick;
+		message = bla.message;
+		vector = bla.vector;
+	}
 }
--- Server/com.ryubai.sgnfexample/src/com/ryubai/sgnfexample/ServerExample.java
+++ Server/com.ryubai.sgnfexample/src/com/ryubai/sgnfexample/ServerExample.java
@@ -1,4 +1,5 @@
 package com.ryubai.sgnfexample;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -7,161 +8,135 @@ import com.ryubai.sgnf.scenarioserver.SSCallHandler;
 import com.ryubai.sgnf.scenarioserver.SSOUT;
 import com.ryubai.sgnf.scenarioserver.SSSocketModel;
 import com.ryubai.sgnf.scenarioserver.ScenarioServer;
+import com.ryubai.sgnf.scenarioserver.Vector;
 
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelId;
-
+import io.netty.handler.timeout.IdleStateEvent;
 
 public class ServerExample {
 
-	
-	//----------------------------------------------------------IS
-	
-	//模拟登录的指令
+	// ----------------------------------------------------------IS
+
+	// 模拟登录的指令
 	private static int COMMAND_TEST_LOGIN = 0x1000;
-	//模拟登录的数据
-	private static class USERIDENTITY{
+
+	// 模拟登录的数据
+	private static class USERIDENTITY {
 		public static String Name = "MyName";
 		public static String Pass = "12345678";
 	}
-	
-	//给IS配置的SS信息
-		@SuppressWarnings("serial")
-		private static ArrayList<SSInfo> ssinfo = new ArrayList<SSInfo>() {
-			{
-				add(new SSInfo("ss0", "192.168.1.102", 9876));
-			}
-		};
-	
-	
-	//----------------------------------------------------------SS
-	
-	//模拟更新玩家位置的指令
-	private static int COMMAND_UPDATE_PLAYER = 0x1001;
-	//模拟的战场
-	private static class BattleFiled {
-		public static SSSocketModel.Vec playerA;
-		public static SSSocketModel.Vec playerB;
-	}
-	
 
-	public static void main(String[] args) throws Exception{
-		
-		
-		//创建一个资料服务器
+	// 给IS配置的SS信息
+	@SuppressWarnings("serial")
+	private static ArrayList<SSInfo> ssinfo = new ArrayList<SSInfo>() {
+		{
+			add(new SSInfo("ss0", "192.168.1.102", 9876));
+		}
+	};
+
+	// ----------------------------------------------------------SS
+
+	// 模拟更新玩家位置的指令
+	private static int COMMAND_UPDATE_PLAYER = 0x1001;
+
+	// 模拟的战场
+	private static class BattleFiled {
+		public static float[] playerA;
+		public static float[] playerB;
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		// 创建一个资料服务器
 		InfoServer is = new InfoServer();
 		is.setSSInfo(ssinfo);
-		is.setCallHandler(new ISCallHandler(){
-			
-			//这个函数处理玩家发来的数据
+		is.setCallHandler(new ISCallHandler() {
+
+			// 这个函数处理玩家发来的数据
 			@Override
-			public ISSocketModel dealMsg(ChannelId id, ISSocketModel message){
+			public ISSocketModel dealMsg(ChannelId id, ISSocketModel message) {
 				ISSocketModel response = new ISSocketModel();
 				response.message.add("");
-				if(message.command == COMMAND_TEST_LOGIN){
+				if (message.command == COMMAND_TEST_LOGIN) {
 					response.command = COMMAND_TEST_LOGIN;
-					
-					if(message.message.get(0).equals(ServerExample.USERIDENTITY.Name) && 
-							message.message.get(1).equals(ServerExample.USERIDENTITY.Pass)){
+
+					if (message.message.get(0).equals(ServerExample.USERIDENTITY.Name)
+							&& message.message.get(1).equals(ServerExample.USERIDENTITY.Pass)) {
 						response.message.set(0, "OK!");
-					}
-					else {
+					} else {
 						response.message.set(0, "NOPE!");
 					}
 					return response;
-				}else return null;
+				} else
+					return null;
 			}
-			
-			//玩家连接
+
+			// 玩家连接
 			@Override
 			public void clientJoin(ChannelId id) {
 				ISOUT.WriteConsole("Client join ID:" + id);
 			}
-			//玩家断开
+
+			// 玩家断开
 			@Override
 			public void clientDrop(ChannelId id) {
-				ISOUT.WriteConsole("Client join ID:" + id);
+				ISOUT.WriteConsole("Client drop ID:" + id);
 			}
-			
-			
-			
-			
-			
-			
-			
+
 		});
 		is.setMaxConn(1024);
 		is.setPort(9999);
-		//如果是start()函数则是同步执行，那程序就堵塞了。。。
+		// 如果是start()函数则是同步执行，那程序就堵塞了。。。
 		is.startThread();
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		//创建一个场景同步服务器
+
+		// 创建一个场景同步服务器
 		ScenarioServer ss = new ScenarioServer();
-		ss.setCallHandler(new SSCallHandler(){
+		ss.setCallHandler(new SSCallHandler() {
 			@Override
-			public SSSocketModel dealMsg(ChannelId id,SSSocketModel message){
-				
-				
+			public SSSocketModel dealMsg(ChannelId id, SSSocketModel message) {
+
 				SSOUT.WriteConsole("Client send ID:" + id);
-				
+
 				return message;
 			}
 			@Override
 			public SSSocketModel tickSend(ChannelId id){
-				SSSocketModel send = new SSSocketModel();
-				send.command = COMMAND_UPDATE_PLAYER;
-				send.vector.add(send.new Vec());
-				//SSOUT.WriteConsole("TICK");
-				return send;
+				SSSocketModel sm = new SSSocketModel();
+				sm.command = COMMAND_UPDATE_PLAYER;
+				sm.vector.add(new Vector(-1,0,0,0));
+				return sm;
 			}
-			//玩家连接
+
+			// 玩家连接
 			@Override
 			public void clientJoin(ChannelId id) {
 				SSOUT.WriteConsole("Client join ID:" + id);
 			}
-			//玩家断开
+
+			// 玩家断开
 			@Override
 			public void clientDrop(ChannelId id) {
-				SSOUT.WriteConsole("Client join ID:" + id);
+				SSOUT.WriteConsole("Client drop ID:" + id);
 			}
-			
+
 		});
-		
-		
+
 		ss.setMaxConn(1024);
 		ss.setTick(30);
 		ss.setPort(ssinfo.get(0).Port);
 		ss.startThread();
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		//监听输入，进行一些有意思的测试
+
+		// 监听输入，进行一些有意思的测试
 		Scanner scanner = new Scanner(System.in);
-		while(true)
-		{
-			String str = scanner.nextLine();  
-			if(str.equals("q")){
+		while (true) {
+			String str = scanner.nextLine();
+			if (str.equals("q")) {
 				is.shut();
 				ss.shut();
 				break;
-			}else{
-				//比如这里修改了密码
+			} else {
+				// 比如这里修改了密码
 				ServerExample.USERIDENTITY.Pass = str;
 			}
 		}
